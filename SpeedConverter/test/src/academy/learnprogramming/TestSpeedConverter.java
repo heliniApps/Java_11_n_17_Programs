c84a504bed0a9bf4347e080cc53abe20414ce960
HeliniK <h.l.kulasuriya@gmail.com>
2022-09-01 00:14:48 +1000

    Merge pull request #1
    * Configuring JUnit to add test cases.

---
+++ SpeedConverter/test/src/academy/learnprogramming/TestSpeedConverter.java
@@ -0,0 +1,26 @@
+package academy.learnprogramming;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestSpeedConverter {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void toMilesPerHour() {
+    }
+
+    @Test
+    void printConversion() {
+    }
+}
