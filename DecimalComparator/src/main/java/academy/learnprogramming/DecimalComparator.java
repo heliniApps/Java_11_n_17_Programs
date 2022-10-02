84083a143533dabcfe174f480d461d9f135c039b
Helini Kulasuriya <h.l.kulasuriya@gmail.com>
2022-10-02 12:50:44 +1100

    Added decimalValueComparator() generic function with a customisable 'scale' value.

--- DecimalComparator/src/main/java/academy/learnprogramming/DecimalComparator.java
+++ DecimalComparator/src/main/java/academy/learnprogramming/DecimalComparator.java
@@ -11,14 +11,8 @@ public class DecimalComparator {
     * */
     public static boolean areEqualByThreeDecimalPlaces(double value1, double value2) {
 
-        if (Double.valueOf(value1).isNaN() || Double.valueOf(value2).isNaN()) {
-            return false;
-        }
+        return decimalValueComparator(value1, value2, 3, RoundingMode.DOWN);
 
-        BigDecimal bdValue1 = BigDecimal.valueOf(value1).setScale(3, RoundingMode.DOWN);
-        BigDecimal bdValue2 = BigDecimal.valueOf(value2).setScale(3, RoundingMode.DOWN);
-
-        return (bdValue1.equals(bdValue2));
     }
 
     /*
@@ -91,4 +85,26 @@ public class DecimalComparator {
         return (compareValue1.trim().contentEquals(compareValue2.trim()));
     }
 
+    private static boolean decimalValueComparator(
+            double value1,
+            double value2,
+            int decimalScale,
+            RoundingMode roundingMode) {
+
+        if (Double.valueOf(value1).isNaN() || Double.valueOf(value2).isNaN()) {
+            return false;
+        }
+        if (decimalScale < 0) {
+            return false;
+        }
+        if (roundingMode == null){
+            return false;
+        }
+
+        BigDecimal bdValue1 = BigDecimal.valueOf(value1).setScale(decimalScale, roundingMode);
+        BigDecimal bdValue2 = BigDecimal.valueOf(value2).setScale(decimalScale, roundingMode);
+
+        return (bdValue1.equals(bdValue2));
+
+    }
 }
