dd5039fa680d517a198a15ef10d25e20acf0a1e1
Helini Kulasuriya <h.l.kulasuriya@gmail.com>
2022-10-09 03:51:18 +1100

    Exercise 37: OOP concepts. Composition. Task related to Has-A relationship.

---
+++ CompositionExercise/src/test/java/com/academy/BedroomTest.java
@@ -0,0 +1,43 @@
+package com.academy;
+
+import org.junit.jupiter.api.Test;
+
+
+class BedroomTest {
+
+    @Test
+    void getLamp() {
+        Wall wall1 = new Wall("West");
+        Wall wall2 = new Wall("East");
+        Wall wall3 = new Wall("South");
+        Wall wall4 = new Wall("North");
+
+        Ceiling ceiling = new Ceiling(12, 55);
+
+        Bed bed = new Bed("Modern", 4, 3, 2, 1);
+
+        Lamp lamp = new Lamp("Classic", false, 75);
+
+        Bedroom bedRoom = new Bedroom("Helini", wall1, wall2, wall3, wall4, ceiling,bed, lamp);
+        bedRoom.getLamp().turnOn();
+    }
+
+    @Test
+    void makeBed() {
+
+        Wall wall1 = new Wall("West");
+        Wall wall2 = new Wall("East");
+        Wall wall3 = new Wall("South");
+        Wall wall4 = new Wall("North");
+
+        Ceiling ceiling = new Ceiling(12, 55);
+
+        Bed bed = new Bed("Modern", 4, 3, 2, 1);
+
+        Lamp lamp = new Lamp("Classic", false, 75);
+
+        Bedroom bedRoom = new Bedroom("Helini", wall1, wall2, wall3, wall4, ceiling,bed, lamp);
+
+        bedRoom.makeBed();
+    }
+}
