bc63de024c5decda48c5e9fb424063e71970dfea
Helini Kulasuriya <h.l.kulasuriya@gmail.com>
2022-11-03 04:27:58 +1100

    Fixed an issue with the bounded type parameters in subclasses of Team class, to only accept the relevant Player types.

--- LeagueTable/src/main/java/com/academy/team/BaseballTeam.java
+++ LeagueTable/src/main/java/com/academy/team/BaseballTeam.java
@@ -1,8 +1,8 @@
 package main.java.com.academy.team;
 
-import main.java.com.academy.player.Player;
+import main.java.com.academy.player.BaseballPlayer;
 
-public class BaseballTeam<T extends Player> extends Team<T> {
+public class BaseballTeam<T extends BaseballPlayer> extends Team<T> {
 
     public BaseballTeam(String name, double score, int gamesPlayed) {
         super(name, score, gamesPlayed);
