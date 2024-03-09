abf66e24b3aecd05c5b650610fa33334e6d1f291
Helini Kulasuriya <h.l.kulasuriya@gmail.com>
2024-03-10 01:10:28 +1100

    MegaBytes Converter -> Convert and print the number of MegaBytes and remaining KiloBytes, from a given number of KiloBytes. Unit Tested using Junit test cases. Implemented following TDD.
    [Exercise 3 - Java 17]

---
+++ J17-Methods_and_Method_Overloading/Challenges/MegaBytesConverter/src/test/com/academy/TestMegaBytesConverter.java
@@ -0,0 +1,48 @@
+package com.academy;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+class TestMegaBytesConverter {
+
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(new PrintStream(outStreamCaptor));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(standardOut);
+    }
+
+    @Test
+    void printMegaBytesAndKiloBytes() {
+        MegaBytesConverter.printMegaBytesAndKiloBytes(-1024);
+        Assertions.assertEquals("Invalid Value", outStreamCaptor.toString().trim());
+        outStreamCaptor.reset();
+
+        MegaBytesConverter.printMegaBytesAndKiloBytes(-1);
+        Assertions.assertEquals("Invalid Value", outStreamCaptor.toString().trim());
+        outStreamCaptor.reset();
+
+        MegaBytesConverter.printMegaBytesAndKiloBytes(0);
+        Assertions.assertEquals("0 KB = 0 MB and 0 KB", outStreamCaptor.toString().trim());
+        outStreamCaptor.reset();
+
+        MegaBytesConverter.printMegaBytesAndKiloBytes(2500);
+        Assertions.assertEquals("2500 KB = 2 MB and 452 KB", outStreamCaptor.toString().trim());
+        outStreamCaptor.reset();
+
+        MegaBytesConverter.printMegaBytesAndKiloBytes(5000);
+        Assertions.assertEquals("5000 KB = 4 MB and 904 KB", outStreamCaptor.toString().trim());
+        outStreamCaptor.reset();
+    }
+}
