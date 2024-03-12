67536615ec1eeba7e40de25ce736609a4d7302ba
Helini Kulasuriya <h.l.kulasuriya@gmail.com>
2024-03-12 23:17:24 +1100

    Changed the lambda expression into a method reference, which was used as the Predicate argument.
    Added alternative approaches for the "hasTeen" method's logic using,
        Arrays.asList()
        List.of()
        IntStream.of()
    
    [ Exercise 8 - Java 17]

--- J17-Methods_and_Method_Overloading/Challenges/TeenNumberChecker/src/test/java/com/academy/TestTeenNumberChecker.java
+++ J17-Methods_and_Method_Overloading/Challenges/TeenNumberChecker/src/test/java/com/academy/TestTeenNumberChecker.java
@@ -6,21 +6,6 @@ import org.junit.jupiter.api.Test;
 class TestTeenNumberChecker {
 
     @Test
-    void hasTeen() {
-        Assertions.assertTrue(TeenNumberChecker.hasTeen(11, 12, 13));
-        Assertions.assertTrue(TeenNumberChecker.hasTeen(10, 15, 20));
-        Assertions.assertTrue(TeenNumberChecker.hasTeen(19, 11, 12));
-        Assertions.assertTrue(TeenNumberChecker.hasTeen(-40, -1, 18));
-        Assertions.assertTrue(TeenNumberChecker.hasTeen(0, 14, 0));
-
-        Assertions.assertFalse(TeenNumberChecker.hasTeen(-40, -1, -18));
-        Assertions.assertFalse(TeenNumberChecker.hasTeen(-19, 10, 25));
-        Assertions.assertFalse(TeenNumberChecker.hasTeen(-19, -13, 25));
-        Assertions.assertFalse(TeenNumberChecker.hasTeen(0, 0, 0));
-        Assertions.assertFalse(TeenNumberChecker.hasTeen(5, 12, 20));
-    }
-
-    @Test
     void isTeen() {
         Assertions.assertTrue(TeenNumberChecker.isTeen(13));
         Assertions.assertTrue(TeenNumberChecker.isTeen(19));
@@ -35,4 +20,18 @@ class TestTeenNumberChecker {
         Assertions.assertFalse(TeenNumberChecker.isTeen(12));
     }
 
+    @Test
+    void hasTeen() {
+        Assertions.assertTrue(TeenNumberChecker.hasTeen(11, 12, 13));
+        Assertions.assertTrue(TeenNumberChecker.hasTeen(10, 15, 20));
+        Assertions.assertTrue(TeenNumberChecker.hasTeen(19, 11, 12));
+        Assertions.assertTrue(TeenNumberChecker.hasTeen(-40, -1, 18));
+        Assertions.assertTrue(TeenNumberChecker.hasTeen(0, 14, 0));
+
+        Assertions.assertFalse(TeenNumberChecker.hasTeen(-40, -1, -18));
+        Assertions.assertFalse(TeenNumberChecker.hasTeen(-19, 10, 25));
+        Assertions.assertFalse(TeenNumberChecker.hasTeen(-19, -13, 25));
+        Assertions.assertFalse(TeenNumberChecker.hasTeen(0, 0, 0));
+        Assertions.assertFalse(TeenNumberChecker.hasTeen(5, 12, 20));
+    }
 }
