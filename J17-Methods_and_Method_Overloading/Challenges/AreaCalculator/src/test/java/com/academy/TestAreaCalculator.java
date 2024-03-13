e15abc009177c1e1da7b9da39aa3f2bfb5a6f1be
Helini Kulasuriya <h.l.kulasuriya@gmail.com>
2024-03-13 19:12:08 +1100

    Improving the readability of the ternary operator logic.
    [ Exercise 9 - Java 17]

--- J17-Methods_and_Method_Overloading/Challenges/AreaCalculator/src/test/java/com/academy/TestAreaCalculator.java
+++ J17-Methods_and_Method_Overloading/Challenges/AreaCalculator/src/test/java/com/academy/TestAreaCalculator.java
@@ -10,7 +10,7 @@ public class TestAreaCalculator {
         Assertions.assertEquals(78.53981633974483d, AreaCalculator.area(5d));
         Assertions.assertEquals(55.417694409323949d, AreaCalculator.area(4.2d));
         Assertions.assertEquals(-1.0d, AreaCalculator.area(-10d));
-        Assertions.assertEquals(0.0, AreaCalculator.area(0d));
+        Assertions.assertEquals(0.0, AreaCalculator.area(0));
     }
 
     @Test
@@ -22,8 +22,8 @@ public class TestAreaCalculator {
         Assertions.assertEquals(20d, AreaCalculator.area(4d, 5d));
         Assertions.assertEquals(42d, AreaCalculator.area(6d, 7d));
         Assertions.assertEquals(275.6502d, AreaCalculator.area(11.69d, 23.58d));
-        Assertions.assertEquals(0.0, AreaCalculator.area(0d, 23.58d));
-        Assertions.assertEquals(0.0, AreaCalculator.area(56.7d, 0d));
-        Assertions.assertEquals(0.0, AreaCalculator.area(0d, 0d));
+        Assertions.assertEquals(0.0, AreaCalculator.area(0, 23.58d));
+        Assertions.assertEquals(0.0, AreaCalculator.area(56.7d, 0));
+        Assertions.assertEquals(0.0, AreaCalculator.area(0, 0));
     }
 }
