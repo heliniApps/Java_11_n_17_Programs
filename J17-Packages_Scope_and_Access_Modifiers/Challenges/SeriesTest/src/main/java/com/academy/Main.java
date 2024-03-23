27d6201c63033e84fcfaeccf1d629163b74c2ced
Helini Kulasuriya <h.l.kulasuriya@gmail.com>
2024-03-24 04:46:14 +1100

    Imports the "MathSeries.jar" library to test below functions, from "com.academy.math" package. nSum(n) factorial(n) fibonacci(n)

---
+++ J17-Packages_Scope_and_Access_Modifiers/Challenges/SeriesTest/src/main/java/com/academy/Main.java
@@ -0,0 +1,14 @@
+package com.academy;
+
+import com.academy.math.Series;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("nSum(10) = " + Series.nSum(10));    // Output = 55
+
+        System.out.println("factorial(8) = " + Series.factorial(8));    // Output = 40320
+
+        System.out.println("fibonacci(6) = " + Series.fibonacci(6));    // Output = 8
+    }
+}
